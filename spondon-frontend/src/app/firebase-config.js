8fadd31721285c6d30d27157d40eefd49534b0da
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-09-16 03:28:10 +0600

    Added Firebase

---
+++ spondon-frontend/src/app/firebase-config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAi_N6iORIVE4i6eG57M97sSiw1fjjjS8s",
+  authDomain: "spondon-23.firebaseapp.com",
+  projectId: "spondon-23",
+  storageBucket: "spondon-23.appspot.com",
+  messagingSenderId: "857948751232",
+  appId: "1:857948751232:web:83878e45cffbc0ed736d65",
+  measurementId: "G-BHT6BK9VFS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app);
+
